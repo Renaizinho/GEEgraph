31f5649ff0c0ba0ba939b2c2143a1aa8d7d435a0
Renan <renanhottz2003@gmail.com>
2024-03-21 12:31:52 -0300

    Mudança nas legendas e títulos

--- GeeGraphTable.py
+++ GeeGraphTable.py
@@ -53,7 +53,7 @@ button_style = {
 }
 
 app.layout = html.Div(style={'background-color': '#E6E6FA'}, children=[
-    html.H1('Proporção de gases GEE e de sucata', style={'color': '#2E8B57', 'text-align': 'center'}),
+    html.H1('ResponsibleSteel - Requisito 10.6.3c (v. 2.0)', style={'color': '#2E8B57', 'text-align': 'center'}),
     html.Div(style=form_style, children=[
         html.P('Favor utilizar PONTO no lugar da VÍRGULA',
                style={'font-weight': 'bold', 'font-size': '14px', 'text-align': 'center', 'margin-bottom': '10px'}),
@@ -197,7 +197,7 @@ def gerar_grafico(pontos, cores_pontos):
 
     ax.set_title('')
     ax.set_xlabel('Scrape share of metallica input (proportion) - X')
-    ax.set_ylabel('GHG emissions intensity of crude steel - Y')
+    ax.set_ylabel('GHG emissions intensity of crude steel - Y\n(t CO₂/tonne crude steel)')
     ax.grid()
 
     buffer = io.BytesIO()
